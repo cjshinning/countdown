dff330a3ecb62745393c6070c9c933c9e1515728
cjshinning <784319417@qq.com>
2018-07-02 16:05:16 +0800

    面向过程定时器

---
+++ countdown.js
@@ -0,0 +1,29 @@
+var timer = null;
+var countdown = document.getElementById('countdown')
+
+countDown();
+
+timer = setInterval(countDown, 1000);
+
+function countDown(){
+	var nowTime = new Date();
+	console.log(nowTime);
+	var endTime = new Date(2018,6,5,15,0,0);
+	console.log(endTime);
+	var t = Math.floor((endTime - nowTime)/1000);
+	var iD = Math.floor(Math.floor(t/86400));
+	var iH = Math.floor(t%86400/3600);
+	var iM = Math.floor((t%3600/60));
+	var iS = t%60;
+	console.log(t)
+	if(t<=0){
+		clearInterval(timer);
+		countdown.innerHTML = '抢购完毕！';
+	}else{
+		countdown.innerHTML = "距离结束时间还有：" + iD + "天" + addZ(iH) + "小时" + addZ(iM) + "分" + addZ(iS) + "秒";
+	}
+}
+
+function addZ(iNum){
+    return iNum<10 ? "0"+iNum : iNum;
+}
